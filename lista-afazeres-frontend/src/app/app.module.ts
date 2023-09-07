ed4f40f84235d8025fd21b849e79011dae9c4e5e
m4rciin <marcin-2011@live.com>
2023-09-07 14:51:44 -0300

    WIP - Conclusão da fase de configuração do projeto, incluindo bibliotecas que serão utilizadas

--- lista-afazeres-frontend/src/app/app.module.ts
+++ lista-afazeres-frontend/src/app/app.module.ts
@@ -1,14 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 import { AppComponent } from './app.component';
+import { CadastrarItemComponent } from './components/cadastrar-item/cadastrar-item.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CadastrarItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+         path: '',
+         component: HomeComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'cadastrar-item',
+        component: CadastrarItemComponent
+      }
+   ]),
+
+    // Componentes PrimeNG
+    ButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ lista-afazeres-frontend/src/app/components/cadastrar-item/cadastrar-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-item',
+  templateUrl: './cadastrar-item.component.html',
+  styleUrls: ['./cadastrar-item.component.css']
+})
+export class CadastrarItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
